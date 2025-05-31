a2cebabcf5a18f21e39cf1232bc5e92d18fd060b
Tolu <toludavid30@gmail.com>
2025-05-31 08:55:42 +0100

    verify commit2

--- src/Auth/Verify.jsx
+++ src/Auth/Verify.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Verify = () => {
     const {token} = useParams()
-    const baseUrl = "http://localhost:5005/auth/verify"
+    const baseUrl = "https://noderender-dm5d.onrender.com/auth/verify"
+
+    const handleVerify =async () =>{
+        const res = await fetch(baseUrl, {
+            method: "POST",
+            body: JSON.stringify({token}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json()
+        console.log(data);
+        
+    }
+    useEffect(()=>{
+        handleVerify()
+    },[])
+
   return (
     <div>Verify</div>
   )
