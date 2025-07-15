59d14f93f31beaf17030561e55f8f1f253b5dab3
Tolu <toludavid30@gmail.com>
2025-07-15 06:54:34 +0100

    top-product-section commit 2

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -5,28 +5,28 @@ const About = () => {
   return (
     <div>
         <div className="firstSection section">
-          <img src="/public/first section.png" alt="" />
+          <img src="/first section.png" alt="" />
         </div>
         <div className='secondSection section'>
-          <img src="/public/second section.png" alt="" />
+          <img src="/second section.png" alt="" />
         </div>
         <div className='thirdSection section'>
-          <img src="/public/third section.png" alt="" />
+          <img src="/third section.png" alt="" />
         </div>
         <div className='fourthSection section'>
-          <img src="/public/fourth section.png" alt="" />
+          <img src="/fourth section.png" alt="" />
         </div>
         <div className='fifthSection section'>
-          <img src="/public/fifth section.png" alt="" />
+          <img src="/fifth section.png" alt="" />
         </div>
         <div className='sixthSection section'>
-          <img src="/public/sixth section.png" alt="" />
+          <img src="/sixth section.png" alt="" />
         </div>
         <div className='sevenSection section'>
-          <img src="/public/seventh section.png" alt="" />
+          <img src="/seventh section.png" alt="" />
         </div>
         <div className='eightSection section'>
-          <img src="/public/Eighth section.png" alt="" />
+          <img src="/Eighth section.png" alt="" />
         </div>
     </div>
   )
