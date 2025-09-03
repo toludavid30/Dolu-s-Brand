47b621bd25c298f53c18ab58166e9468f02c187e
Tolu <toludavid30@gmail.com>
2025-09-03 11:46:41 +0100

    cart commit

--- src/LandingPage/components/TopProductSection.jsx
+++ src/LandingPage/components/TopProductSection.jsx
@@ -1,11 +1,68 @@
 import React, { useEffect } from 'react'
 import useCart from '../../context/CartContext/component/useCart'
+import { gsap } from "gsap";
+// import { useGSAP } from "@gsap/react";
+
+import { CustomEase } from "gsap/CustomEase";
+// CustomBounce requires CustomEase
+import { CustomBounce } from "gsap/CustomBounce";
+// CustomWiggle requires CustomEase
+import { CustomWiggle } from "gsap/CustomWiggle";
+import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { Power3 } from 'gsap/src';
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother,RoughEase,ExpoScaleEase,SlowMo,CustomEase,CustomBounce,CustomWiggle);
+
 
 const TopProductSection = () => {
     const {isLoggedIn, checkIsLoggedIn, addtoCart} = useCart()
 
     useEffect(()=>{
           checkIsLoggedIn()
+          const tl = gsap.timeline({
+            scrollTrigger: {
+            trigger:".sectionWrapper",
+            start: "top 80%", 
+            end: "bottom 20%",
+            // scrub: true
+
+            }
+            })
+          tl.fromTo(".leftCard", {
+            opacity: 0,
+            x: -300,
+            ease: Power3.easeIn
+          },{
+            duration: 2,
+            opacity: 1,
+            x: 0,
+            },"first")
+
+            tl.fromTo(".rightCard", {
+            opacity: 0,
+            x: 300,
+            ease: Power3.easeIn,
+            display: "none"
+          },{
+            duration: 2,
+            opacity: 1,
+            x: 0,
+            display:"block"
+        },"first")
+
+            tl.fromTo(".centerCard", {
+            opacity: 0,
+            y: 300,
+            ease: Power3.easeIn
+          },{
+            duration: 2,
+            opacity: 1,
+            y: 20,
+        }, "first -=1")
         }, [])
   return (
     <div id='TopProductSection' className='py-4 gap-2'>
@@ -13,7 +70,7 @@ const TopProductSection = () => {
             Top Products
         </h2>
         <div className="sectionWrapper container d-flex flex-wrap justify-content-around">
-            <div className="cardWrapper card w-25">
+            <div className="cardWrapper leftCard card w-25">
                 <img src="/Ankara-Cross, Cargo.jpeg" alt="" className='card-img-top' />
                 <div className="cardText card-body d-flex flex-column justify-content-evenly text-center">
                     <h6 className='card-title'>
@@ -25,7 +82,7 @@ const TopProductSection = () => {
                     <a className='btn btn-sm btn-dark mx-auto' href='/products/403-603'>View Product</a>
                 </div>
             </div>
-            <div className="cardWrapper card w-25">
+            <div className="cardWrapper centerCard card w-25">
                 <img src="/White Native Sample.jpg" alt="" className='card-img-top' />
                 <div className="cardText card-body d-flex flex-column justify-content-evenly text-center" >
                     <h6 className='card-title'>
@@ -50,7 +107,7 @@ const TopProductSection = () => {
                         <a className='btn btn-sm btn-dark mx-auto' href="/products/105">View Product</a>
                 </div>
             </div>
-            <div className="cardWrapper card w-25">
+            <div className="cardWrapper rightCard card w-25">
                 <img src="/TFM Tote.jpeg" alt="" className='card-img-top' />
                 <div className="cardText card-body d-flex flex-column justify-content-evenly text-center">
                     <h6 className='card-title'>
